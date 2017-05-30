143d632abd2fe8adc275a6d837703c6b5f982741
warmdog <973128493@qq.com>
2017-05-30 21:14:34 +0900

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "Web"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+    
---
+++ src/main/scala/DD.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by hadoop on 17/05/28.
+  */
+object DD {
+  def main(args: Array[String]) {
+    print("111111");
+
+    if (2 > 3) {
+      print("daaaaa")
+    } else {
+      print("dddd");
+    }
+  }
+}
